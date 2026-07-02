b4b25dfc53042bea113bbc9557e7579cbe2b2597
he520373 <he520373@github.com>
2026-07-02 11:20:54 +0800

    fix: 显示镜像修正-swap_xy+mirror_x

--- main/boards/xingzhi-cube-1.54tft-wifi/config.h
+++ main/boards/xingzhi-cube-1.54tft-wifi/config.h
@@ -25,7 +25,7 @@
 #define DISPLAY_WIDTH   240
 #define DISPLAY_HEIGHT  240
 #define DISPLAY_SWAP_XY  true
-#define DISPLAY_MIRROR_X false
+#define DISPLAY_MIRROR_X true
 #define DISPLAY_MIRROR_Y false
 #define BACKLIGHT_INVERT false
 #define DISPLAY_OFFSET_X  0
